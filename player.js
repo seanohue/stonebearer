46e30d042462cf75b1c24ae2dc1a45e067e8cda1
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-12-17 12:20:53 -0500

    Add handling for picking up item if inventory slot is empty or backpack is empty.

--- player.js
+++ player.js
@@ -4,6 +4,20 @@ var Player = module.exports = function (x, y) {
     this._x = x;
     this._y = y;
     this._draw();
+
+    this.inventory = {
+        backpack: null,
+        held: {
+            name: 'a torch',
+            location: 'held'
+        },
+        body: {
+            name: 'some rags',
+            location: 'body'
+        },
+        head: null,
+        stones: []
+    }
 }
 
 Player.prototype.getSpeed = function () {
@@ -16,8 +30,20 @@ Player.prototype.getY = function () {
     return this._y;
 }
 
-Player.prototype.act = function () {
-    Game.engine.lock();
-    process.stdin.on("keypress", this.handleEvent);
+Player.prototype.getInventory = function (key) {
+    if (key) return this.inventory[key];
+    return this.inventory;
 }
 
+Player.prototype.addToInventory = function (item) {
+    if (!this.inventory[item.location]) {
+        this.inventory[item.location] = item;
+        return "You equip " + item.name + ".";
+    } else if (!this.inventory.backpack) {
+        this.inventory.backpack = item;
+        return "You put " + item.name + " in your backpack.";
+    } else {
+        // do a thing where you ask if they want to swap it?
+        // if backpack is empty, ask if they want to store it there (one item at a time)
+    }
+}
