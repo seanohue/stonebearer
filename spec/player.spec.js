67d35364547a266b179d1bd313bf0662c8b3ce9a
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2015-12-28 00:35:27 -0500

    Inventory stringification test

--- spec/player.spec.js
+++ spec/player.spec.js
@@ -2,13 +2,14 @@ var common = require('../common');
 var Player = require('../player');
 var expect = common.expect;
 
-var testPlayer
+var testPlayer;
+
+beforeEach(function() {
+    testPlayer = new Player(42, 420);
+    console.log(testPlayer);
+});
 
 describe("Attributes", function() {
-    beforeEach(function() {
-        testPlayer = new Player(42, 420);
-        console.log(testPlayer);
-    });
 
     it("Returns all of the attributes if there are no arguments", function() {
         var attr = testPlayer.getAttributes();
@@ -21,6 +22,20 @@ describe("Attributes", function() {
     it("Returns an integer attribute when the key to that attribute is passed in.", function() {
         var attr = testPlayer.getAttributes('defense');
         expect(attr).to.eql(2);
+    });
+
+});
+
+describe("Inventory", function() {
+    beforeEach(function() {
+
+    });
+
+    it("Returns all of the inventory if there are no arguments", function() {
+        var inv = testPlayer.getInventory();
+        var expectedInv = "Inventory: \nfeet: a pair of sandals\nheld: a torch\nbody: some rags\n";
+
+        expect(inv).to.eql(expectedInv);
     });
 
 });
