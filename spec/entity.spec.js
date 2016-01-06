840414812bbf539c87d14e9e71f64acd93502a75
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-01-05 22:16:39 -0500

    Tests for default initialization of entity.

--- spec/entity.spec.js
+++ spec/entity.spec.js
@@ -9,8 +9,32 @@ var drawStub = function() {
 describe("Initialization of entity", function() {
 
     var testEntity = new Entity(42, 420, drawStub);
-    it("should have the right speed", function() {
+    it("should be able to get speed for the scheduler", function() {
         expect(testEntity.getSpeed()).to.equal(100);
+    });
+
+    it("should have initialized with default settings", function() {
+        var defaultEntity = {
+            name: "beast",
+
+            attributes: {
+                speed: 100,
+                health: 10,
+                damage: 2,
+                defense: 2
+            },
+            symbol: "&",
+            color: "red",
+
+            action: function() {},
+        };
+
+        expect(testEntity.attributes).to.eql(defaultEntity.attributes);
+        expect(testEntity._name).to.equal(defaultEntity.name);
+        expect(testEntity._color).to.equal(defaultEntity.color);
+        expect(testEntity._symbol).to.equal(defaultEntity.symbol);
+        expect(testEntity._draw).to.equal(drawStub);
+
     });
 
 })
