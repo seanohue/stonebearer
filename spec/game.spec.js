570830ce1d7b5bc4289b1c3cb7378310d1bf985f
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-01-10 15:39:01 -0500

    Add gulp for various tasks, add comments about possible blockers in testing game module.

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -5,7 +5,10 @@ var Entity = require('../entity');
 var Loot = require('../loot');
 var Lore = require('../lore');
 
-describe("Does things", function() {
-    it("should also do stuff", function() {
-    });
+describe("Player event handling", function() {
+    var testPlayer = new Player(10, 10);
+
+    // TODO: find a way to mock keypress events (stdin?)
+    // TODO: add sinon to stub out methonds like stdout to see if they are called as needed.
+    
 });
