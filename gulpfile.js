a6535c90e1ed4eacef0df3190429b20bf6bb2ed1
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-01-10 16:19:24 -0500

    Install load-plugins and todo, add todo task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var $ = rquire('gulp-load-plugins')();
 
 gulp.task('default', function(){
 	console.log("GULP");
+});
+
+gulp.task('todo', function() {
+    gulp.src('js/**/*.js')
+        .pipe($.todo())
+        .pipe(gulp.dest('./'));
 });
