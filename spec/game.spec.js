457687d790f93dc2bc5f6d14a7d190b9899ce496
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-01-11 16:38:43 -0500

    Add entity init tests, add beforeEach for game.init, add describe block for show message.

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -7,8 +7,9 @@ var Lore = require('../lore');
 var ROT = require('rot-js');
 var expect = common.expect;
 
+beforeEach(Game.init);
+
 describe("Game initialization station", function() {
-    Game.init();
 
     it("should have a display", function() {
         expect(Game.display).to.be.instanceof(ROT.Display);
@@ -20,19 +21,29 @@ describe("Game initialization station", function() {
 
     describe("Entity group initialization", function() {
 
-    });
+        it("should have an array of entity objects", function() {
+            expect(Game._entities).to.be.an.Array;
 
-    it("should have an array of entity objects", function() {
-        console.log("ENTITIES: ", Game._entities);
-        console.log("How many?", Game._entities.length);
-        expect(Game._entities).to.be.an.Array;
-        
-        //TODO: create mockEntity to put in mochaHelper.js to help with testing entities here and in the entity spec.
-        for (var item in Game._entities){
-        	var entity = Game._entities[item];
-        	expect(entity).to.be.an.Object;
-        }
+            //TODO: create mockEntity to put in mochaHelper.js to help with testing entities here and in the entity spec.
+            for (var item in Game._entities) {
+                var entity = Game._entities[item];
+                expect(entity).to.be.an.Object;
+            }
+
+            it("should create a specific number of entities", function() {
+                expect(Game._entities.length).to.equal(22);
+            });
+        });
     });
+});
+
+describe("ShowMessage method", function(){
+	it("does message stuff", function(){
+
+	});
+})
+
+
 
     // TODO: find a way to mock keypress events (stdin?)
     // TODO: add sinon to stub out methonds like stdout to see if they are called as needed.
