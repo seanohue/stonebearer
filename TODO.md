a46d8fb56f32864c3162d4bf9b7669458bcb9f20
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-01-11 11:46:04 -0500

    Fix path for todo. Extract paths for gulp tasks into variables. Add jshint task and extract tasks into function

---
+++ TODO.md
@@ -0,0 +1,5 @@
+### TODOs
+| Filename | line # | TODO
+|:------|:------:|:------
+| /Users/seanodonohue/myProjects/rlp/game.js | 62 | Use stuff like this for making menus cleaner
+| /Users/seanodonohue/myProjects/rlp/game.js | 156 | Break handleEvent into various functions.
--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,28 @@ var options = {
     }
 };
 
-gulp.task('default', function() {
-    console.log("GULP");
-});
+var paths = {
+    src: './',
+    js: './*.js'
+}
 
-gulp.task('todo', function() {
-    gulp.src('js/**/*.js')
+gulp.task('default', ['todo', 'jshint'], defaultTask);
+gulp.task('jshint', jsHintTask);
+gulp.task('todo', toDoTask);
+
+
+function defaultTask() {
+    console.log("GULP");
+}
+
+function jsHintTask() {
+    gulp.src(paths.js)
+        .pipe($.jshint());
+}
+
+function toDoTask() {
+    gulp.src(paths.js)
         .pipe($.todo(options.todo))
-        .pipe(gulp.dest('./'));
-});
+        .pipe(gulp.dest(paths.src));
+}
+
