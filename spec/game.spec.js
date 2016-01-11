debe284c2ada05afd625f05d42a00877585add23
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-01-11 15:57:46 -0500

    Add tests for game init and entities init

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -4,15 +4,37 @@ var Player = require('../player');
 var Entity = require('../entity');
 var Loot = require('../loot');
 var Lore = require('../lore');
+var ROT = require('rot-js');
+var expect = common.expect;
 
 describe("Game initialization station", function() {
     Game.init();
 
-    it("", function(){
+    it("should have a display", function() {
+        expect(Game.display).to.be.instanceof(ROT.Display);
+    });
 
+    it("should have an engine", function() {
+        expect(Game.engine).to.be.instanceof(ROT.Engine);
+    });
+
+    describe("Entity group initialization", function() {
+
+    });
+
+    it("should have an array of entity objects", function() {
+        console.log("ENTITIES: ", Game._entities);
+        console.log("How many?", Game._entities.length);
+        expect(Game._entities).to.be.an.Array;
+        
+        //TODO: create mockEntity to put in mochaHelper.js to help with testing entities here and in the entity spec.
+        for (var item in Game._entities){
+        	var entity = Game._entities[item];
+        	expect(entity).to.be.an.Object;
+        }
     });
 
     // TODO: find a way to mock keypress events (stdin?)
     // TODO: add sinon to stub out methonds like stdout to see if they are called as needed.
-    
+
 });
