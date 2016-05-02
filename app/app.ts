2cafb013021a66f42103903cfb93b2f7c418845d
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2016-05-02 14:17:53 -0400

    Code cleanup with named variables :fire:

--- app/app.ts
+++ app/app.ts
@@ -2,23 +2,32 @@
 
 class SimpleGame {
 
-    constructor() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
-    }
+  constructor() {
+    const width = 800;
+    const height = 600;
+    const renderer = Phaser.AUTO;
+    const parentElement = 'content';
+    const gameStates = {
+      preload: this.preload,
+      create: this.create,
+    };
 
-    game: Phaser.Game;
+    this.game = new Phaser.Game(width, height, renderer, parentElement, gameStates);
+  }
 
-    preload() {
-        this.game.load.image('logo', 'phaser2.png');
-    }
+  game: Phaser.Game;
 
-    create() {
-        const logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
-    }
+  preload() {
+      this.game.load.image('logo', 'phaser2.png');
+  }
+
+  create() {
+      const logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+      logo.anchor.setTo(0.5, 0.5);
+  }
 
 }
 
 window.onload = () => {
-    var game = new SimpleGame();
+  var game = new SimpleGame();
 };
